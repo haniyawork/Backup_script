fc554d57010c5076d208431ffe99297baebb43ab
haniyawork <haniyeh.work.a@gmail.com>
2024-11-26 05:26:05 -0800

    Send backup log to Telegram

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
 #Variables
-#DB_NAME="TEST"
-#DB_USER="USER"
-#DB_PASS="PASSWORD"
+
 BACKUP_DIR="/var/backups/Databases_Backup"
 DATE=$(date +%F)
-#BACKUP_FILE="${DATE}.sql"
+LOG_FILE="$BACKUP_DIR/backup.log"
+TELEGRAM_BOT_TOKEN="your_bot_token"
+TELEGRAM_CHAT_ID="your_chat_id"
 
 cd /var/backups/Databases_Backup
 mkdir -p "$BACKUP_DIR"
+
 #Backup Script for all databases
 for DB in $(mysql -e 'show databases' -s --skip-column-names); do
 	mysqldump $DB | gzip -c > "${DB}-${DATE}.sql.gz"
 done
 
 #Checking
+
 if [ $? -eq 0 ]; then
-    echo "Backup for all databases completed successfully at $BACKUP_DIR."
+    MESSAGE="DATABASES Backup completed successfully: $DATE"
+    echo "$(date) - SUCCESS - $MESSAGE" >> "$LOG_FILE"
 else
-    echo "Backup for all databases failed."
+    MESSAGE="DATABASES Backup failed for database: $DB"
+    echo "$(date) - ERROR - $MESSAGE" >> "$LOG_FILE"
 fi
+
+# Send notification to Telegram
+curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+    -d chat_id="$TELEGRAM_CHAT_ID" \
+    -d text="$MESSAGE"
